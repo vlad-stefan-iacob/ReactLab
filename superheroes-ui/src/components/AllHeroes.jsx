b48cf64ea4ae3c09c9d1b2210160af3dd622c637
Vlad Stefan Iacob <vladiacob14@gmail.com>
2023-08-01 14:10:21 +0100

    final version

--- superheroes-ui/src/components/AllHeroes.jsx
+++ superheroes-ui/src/components/AllHeroes.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import HeroDetail from './HeroDetail';
 import Row from 'react-bootstrap/Row'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getAllHeroes } from '../services/hero-service';
 
 const AllHeroes = () => {
     const [total, setTotal] = useState(0);
+    const [heroes,setHeroes] = useState([]);
 
     const totalCountHandler = (name) =>{
         console.log(name)
         setTotal(total+1);       
     }
 
-    const heroes = [
-        {"id":1,"name":"Bruce Wayne","alias":"Batman","superpower":"Martial Arts","teamID":1},
-        {"id":2,"name":"Clark Kent","alias":"Superman","superpower":"Flight","teamID":1},
-        {"id":3,"name":"Jay Garrick","alias":"The Flash","superpower":"Super-speed","teamID":2},
-        {"id":4,"name":"Alan Scott","alias":"Green Lantern","superpower":"Ring Creation","teamID":2},
-        {"id":5,"name":"Helena Bertenelli","alias":"The Huntress","superpower":"Crossbow Archery","teamID":3},
-        {"id":6,"name":"Dr. Harleen Quinzel","alias":"Harley Quinn","superpower":"Hammer-fighting","teamID":3},
-        {"id":7,"name":"Floyd Lawton","alias":"Deadshot","superpower":"Marksmaship","teamID":4},
-        {"id":8,"name":"Cecil Adams","alias":"Count Vertigo","superpower":"Drug-dealing","teamID":4},
-        {"id":9,"name":"Damian Wayne","alias":"Robin","superpower":"Swordsmanship","teamID":5},
-        {"id":10,"name":"Dick Grayson","alias":"Nightwing","superpower":"Acrobatics","teamID":5}
-    ]
+    useEffect(()=>{
+        getHeroesFromAPI();}, 
+        []
+    );
+    
+    const getHeroesFromAPI = () => {
+        getAllHeroes()
+            .then(res => {
+                setHeroes(res.data);
+            })
+            .catch(err => {
+                setHeroes([]);
+                console.log(err);
+            })
+    }
+
+    // const heroes = [
+    //     {"id":1,"name":"Bruce Wayne","alias":"Batman","superpower":"Martial Arts","teamID":1},
+    //     {"id":2,"name":"Clark Kent","alias":"Superman","superpower":"Flight","teamID":1},
+    //     {"id":3,"name":"Jay Garrick","alias":"The Flash","superpower":"Super-speed","teamID":2},
+    //     {"id":4,"name":"Alan Scott","alias":"Green Lantern","superpower":"Ring Creation","teamID":2},
+    //     {"id":5,"name":"Helena Bertenelli","alias":"The Huntress","superpower":"Crossbow Archery","teamID":3},
+    //     {"id":6,"name":"Dr. Harleen Quinzel","alias":"Harley Quinn","superpower":"Hammer-fighting","teamID":3},
+    //     {"id":7,"name":"Floyd Lawton","alias":"Deadshot","superpower":"Marksmaship","teamID":4},
+    //     {"id":8,"name":"Cecil Adams","alias":"Count Vertigo","superpower":"Drug-dealing","teamID":4},
+    //     {"id":9,"name":"Damian Wayne","alias":"Robin","superpower":"Swordsmanship","teamID":5},
+    //     {"id":10,"name":"Dick Grayson","alias":"Nightwing","superpower":"Acrobatics","teamID":5}
+    // ]
     return (
         <>
             <h1> Total count is: {total}</h1>    
--- superheroes-ui/src/components/HeroForm.jsx
+++ superheroes-ui/src/components/HeroForm.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { saveHero } from '../services/hero-service'
 
 const HeroForm = () => {
+    const [alias, setAlias] = useState('');
+    const [name, setName] = useState('');
+    const [ability, setAbility] = useState('');
+    const [teamID, setTeamID] = useState(0);
+
+    const navigate = useNavigate();
+
+    const handleAliasChange = (event)=> {
+        setAlias(event.target.value);
+    }
+
+    const handleNameChange = (event)=>{
+        setName(event.target.value);
+    }
+
+    const handleAbilityChange = (event)=>{
+        setAbility(event.target.value)
+    }
+
+    const handleTeamChange = (event)=>{
+        setTeamID(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        let hero = {};
+        hero.alias = alias;
+        hero.name = name;
+        hero.superpower = ability;
+        hero.teamID = teamID;
+        console.log(hero);
+    
+        saveHero(hero)
+       .then(res => {
+          setAbility('');
+          setAlias('');
+          setName('');
+          setTeamID(0);
+          navigate("/");
+          })
+        .catch(err=>{
+           console.log(err);
+          })
+      }
+
   return (
-    <div>HeroForm</div>
+    <Row className='heroForm'>
+        <Form>
+            <Form.Group className="mb-3" controlId="alias">
+                <Form.Label>Hero Alias: </Form.Label>
+                <Form.Control type="text" placeholder="Hero alias" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Hero Name: </Form.Label>
+                <Form.Control type="text" placeholder="Hero name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="ability">
+                <Form.Label>Hero Ability: </Form.Label>
+                <Form.Control type="text" placeholder="Hero ability" />
+            </Form.Group>
+            <Form.Select aria-label="Team ID">
+                <option>Team ID</option>    
+                <option value="1">One</option>    
+                <option value="2">Two</option>    
+                <option value="3">Three</option>  
+            </Form.Select>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
+        </Form>
+    </Row>
   )
 }
 
